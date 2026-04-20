a54ca2857deec1d4e5691b8374a93817d65a9ff5
DIMENEID <muzaqi.nurar4@gmail.com>
2026-04-20 11:58:05 +0700

    neww

---
+++ src/main/java/com/mycompany/presensikonstruksi_rfid/PresensiKonstruksi_RFID.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.presensikonstruksi_rfid;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class PresensiKonstruksi_RFID {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
